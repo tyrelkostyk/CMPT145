cfaa6581678d23cc5d51d73179e89864051a9b14
tyrelkostyk <tck290@mail.usask.ca>
2018-02-06 10:26:46 -0600

    Finished 1st draft fof a4q1.py, almost works

--- Ass_04/a4q1.py
+++ Ass_04/a4q1.py
@@ -1,3 +1,25 @@
 ## Tyrel Kostyk, tck290, 11216033
 ## CMPT145-04, Lab Section 04
 ## a4, due Friday Feb 9th 10pm
+
+import TStack as Stack
+import sys
+
+
+lineStack = Stack.create()
+wordStack = Stack.create()
+
+file = open(sys.argv[1])
+
+for line in file:
+    Stack.push(lineStack, line)
+
+while not Stack.is_empty(lineStack):
+    line = Stack.pop(lineStack)
+    x = line.split()
+    for words in x:
+        Stack.push(wordStack, words)
+    while not Stack.is_empty(wordStack):
+        print(Stack.pop(wordStack))
+
+file.close()
