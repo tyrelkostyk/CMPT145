371eb762ab605a0ff814f870c8d3b4ad4569b3ba
tyrelkostyk <tck290@mail.usask.ca>
2018-02-06 10:36:43 -0600

    Finished 2nd draft, found and fixed printing mistake

--- Ass_04/a4q1.py
+++ Ass_04/a4q1.py
@@ -5,21 +5,26 @@
 import TStack as Stack
 import sys
 
-
+# Initialize Stacks
 lineStack = Stack.create()
 wordStack = Stack.create()
 
+# Read file
 file = open(sys.argv[1])
 
+# Push each line onto lineStack
 for line in file:
     Stack.push(lineStack, line)
 
+# Reverse the order of words w/in each line, then print each line in reverse order
 while not Stack.is_empty(lineStack):
+    result = ''
     line = Stack.pop(lineStack)
     x = line.split()
     for words in x:
         Stack.push(wordStack, words)
     while not Stack.is_empty(wordStack):
-        print(Stack.pop(wordStack))
+        result += str(Stack.pop(wordStack))+' '
+    print(result)
 
 file.close()
