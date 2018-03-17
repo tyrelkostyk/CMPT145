fcc515f6b63e1dc0cd521271d6ee988d7e57cdd4
tyrelkostyk <tck290@mail.usask.ca>
2018-03-16 21:48:49 -0600

    Finished a6q5.py, all functions tested & working as expected

--- Ass_07/a7q5.py
+++ Ass_07/a7q5.py
@@ -10,6 +10,7 @@ def fibonacci(n):
     n - non-negative integer
     :returns: the value of the nth fibonacci number
     '''
+    # base case
     if (n == 0) or (n == 1):
         return n
     elif n > 1:
@@ -23,7 +24,27 @@ def moosonacci(n):
     n - non-negative integer
     :returns: the value of the nth moosonacci number
     '''
+    # base case
     if n in (0,1,2):
         return n
     elif n > 2:
         return moosonacci(n-1) + moosonacci(n-2) + moosonacci(n-3)
+
+# c)
+def substr(s, c, r):
+    '''
+    composes new string, identical to s except every instance of c replaced with r
+    :params:
+    s - string, which you wish to edit
+    c - string, "target" which you wish to replace with r
+    r - string, will replace c
+    :returns: the new string
+    '''
+    # base case
+    if s.count(c) == 0:
+        return s
+    else:
+        if s[0] == c:
+            return r + substr(s[1:len(s)],c,r)
+        else:
+            return s[0] + substr(s[1:len(s)],c,r)
