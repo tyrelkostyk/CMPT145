2a4fb0e2987b2313abce427b1950f50d39f76a33
tyrelkostyk <tck290@mail.usask.ca>
2018-03-16 20:29:50 -0600

    Finished non-programming questions for a7

---
+++ Ass_07/a7q6.py
@@ -0,0 +1,3 @@
+## Tyrel Kostyk, tck290, 11216033
+## CMPT145-04, Lab Section 04
+## A7q6; due Friday Saturday March 17th, 2018, 10pm
---
+++ Ass_07/a7q7.py
@@ -0,0 +1,3 @@
+## Tyrel Kostyk, tck290, 11216033
+## CMPT145-04, Lab Section 04
+## A7q7; due Friday Saturday March 17th, 2018, 10pm
