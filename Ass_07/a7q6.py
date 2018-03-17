469646071d9e5911086fd4d42c92123e9ff09954
tyrelkostyk <tck290@mail.usask.ca>
2018-03-17 02:06:22 -0600

    finished writing & testing parts a and c for a7q6.py. b is still eluding me however

--- Ass_07/a7q6.py
+++ Ass_07/a7q6.py
@@ -14,14 +14,53 @@ def subst(chain, t, r):
     r - data value, replaces all occurances of t
     :pre: Chain already initialized as valid Node data type
     :post: all occurances of t are replaced with r, in chain
-    :returns: None
+    :returns: the (now modified) chain
     '''
     # base case
     if chain == None:
         return None
     else:
+        # change target data
         if node.get_data(chain) == t:
             node.set_data(chain, r)
-        next_node = subst(node.get_next(chain), t, r)
-        node.set_next(chain, next_node)
+        # recrusive call
+        subst(node.get_next(chain), t, r)
         return chain
+
+# b)
+def reverse(chain):
+    '''
+    reverses all data in node chain
+    :params:
+    chain - node, initialized & possibly empty
+    :returns: the (now modified) chain
+    '''
+    # base case
+    if chain == None:
+        return chain
+    else:
+        # recursive call
+        node.set_data(node.get_data( reverse(node.get_next(chain)) ))
+
+        return chain
+
+# c)
+def copy(chain):
+    '''
+    produces a new chain, with same structure & values as original chain
+    :params:
+    chain - node, possibly empty
+    :returns: new (identical but seperate) chain
+    '''
+    # special case: empty node
+    if chain == None:
+        return None
+    # base case
+    # elif node.get_next(chain) == None:
+    #     return node.create(node.get_data(chain))
+    else:
+        # new_chain = copy(node.get_next(chain))
+        # node.set_data(new_chain, node.get_data(chain))
+        # node.set_next(new_chain, node.get_next(chain))
+
+        return node.create(node.get_data(chain), next=copy(node.get_next(chain)) )
